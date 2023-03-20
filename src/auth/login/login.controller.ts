a2ab5da888725ffe14397003b14585236b00c84f
Sahil Bhatia <sahil.bhatia@skuad.io>
2023-03-20 18:48:03 +0530

    Added http status code specific to user states

--- src/auth/login/login.controller.ts
+++ src/auth/login/login.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Req,} from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, Req, Res,} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AuthService } from 'src/common/services/auth/auth.service';
+import { HttpStatusCode } from 'axios';
 
 @Controller('auth/login')
 export class LoginController {
@@ -9,22 +10,26 @@ export class LoginController {
   ) {}
 
   @Post()
-  async login(@Req() req: Request){
+  async login(@Req() req: Request, @Res() res: Response){
     console.log("Body Recieved for Authentication ==> ", req.body);
     
     const response = await this.authService.login(req.body.user);
     
     if (response != null && response['user'] && response['accessToken']) {
-      return {
-        status: 'success',
-        ...response
-      }
+      return res
+        .status(response['newUserCreated'] ? HttpStatusCode.Created : HttpStatusCode.Ok)
+        .json({
+          status: 'success',
+          ...response
+        })
     }
 
-    return {
-      status: 'error',
-      message: response?.message || 'Something went wrong!',
-      accessToken: null,
-    }
+    return res
+      .status(HttpStatusCode.Unauthorized)
+      .json({
+        status: 'error',
+        message: response?.message || 'Something went wrong!',
+        accessToken: null,
+      });
   }
 }
