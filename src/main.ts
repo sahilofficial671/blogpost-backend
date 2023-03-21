740fff858c9ccc28db6a6b8651f5a77a29dc2b7b
Sahil Bhatia <sahil.bhatia@skuad.io>
2023-03-21 17:33:00 +0530

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import * as cors from 'cors';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.use(morgan('tiny'))
-  app.enableCors();
+  app.use([morgan('dev'), cors()])
+  // app.enableCors();
 
   await app.listen(process.env.APP_PORT);
 }
