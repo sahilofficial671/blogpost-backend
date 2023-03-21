8bbe877fe8e84b6d732682bfa65f107906bcf0f7
Sahil Bhatia <sahil.bhatia@skuad.io>
2023-03-21 16:08:10 +0530

    Added morgan

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.use(morgan('tiny'))
   app.enableCors();
 
   await app.listen(process.env.APP_PORT);
